363c9591172dd9bdb758b45e5cfb231231de320a
Gabriel Dolce <affairsinternet@gmail.com>
2025-05-18 16:54:34 -0300

    Ejercicio 4 + Mostrar todas las sucursales.

--- TP7_GRUPO_24/GestionSucursales.cs
+++ TP7_GRUPO_24/GestionSucursales.cs
@@ -17,17 +17,73 @@ namespace TP7_GRUPO_24
             sqlParameter = sqlCommand.Parameters.Add("@NombreSucursal", SqlDbType.NVarChar, 100);
             sqlParameter.Value = sucursal.NombreSucursal + "%";
         }
+   
+        public void ParametrosProvincia(SqlCommand sqlCommand, Sucursal sucursal) // Agregado.
+        {
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlCommand.Parameters.Add("@Id_ProvinciaSucursal", SqlDbType.Int).Value = sucursal.Id_ProvinciaSucursal;
+        }
 
         public DataTable FiltrarTabla(Sucursal sucursal)
         {
             SqlCommand sqlCommand = new SqlCommand();
-            string nombreTabla = "Sucursales";
+            string nombreTabla = "Sucursal";
             ParametrosBuscar(sqlCommand, sucursal);
            Conexion conexion = new Conexion();
             string consultasql = "SELECT NombreSucursal, DescripcionSucursal, URL_Imagen_Sucursal, Id_Sucursal FROM Sucursal WHERE NombreSucursal LIKE @NombreSucursal";
             DataTable tabla = conexion.ObtenerTablaFiltrada(sqlCommand, consultasql, nombreTabla);
             return tabla;
+        }
 
+        public DataTable ObtenerTodasLasSucursales() // Agregado.
+        {
+            string consultaSQL = "SELECT NombreSucursal, DescripcionSucursal, URL_Imagen_Sucursal, Id_Sucursal FROM Sucursal";
+            using (SqlConnection con = conexion.ObtenerConexion())
+            {
+                SqlCommand cmd = new SqlCommand(consultaSQL, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable tabla = new DataTable("Sucursal");
+                adapter.Fill(tabla);
+                return tabla;
+            }
+        }
+
+        public DataTable FiltrarPorProvincia(Sucursal sucursal) // Agregado.
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            ParametrosProvincia(sqlCommand, sucursal);
+            string consultaSQL = @"
+        Select
+            S.NombreSucursal, 
+            S.DescripcionSucursal, 
+            S.URL_Imagen_Sucursal, 
+            S.Id_Sucursal, 
+            P.DescripcionProvincia,
+            P.Id_Provincia
+        From 
+            Sucursal S
+        Inner Join
+            Provincia P On S.Id_ProvinciaSucursal = P.Id_Provincia
+        Where 
+            S.Id_ProvinciaSucursal = @Id_ProvinciaSucursal";
+
+            Conexion conexion = new Conexion();
+            string nombreTabla = "Sucursal";
+            DataTable tabla = conexion.ObtenerTablaFiltrada(sqlCommand, consultaSQL, nombreTabla);
+            return tabla;
+        }
+
+        public DataTable LeerConsulta() // Para Botones de DataList.
+        {
+            string consultaSQL = "Select P.Id_Provincia, P.DescripcionProvincia FROM Provincia P";
+            using (SqlConnection con = conexion.ObtenerConexion())
+            {
+            SqlCommand cmd = new SqlCommand(consultaSQL, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable tabla = new DataTable();
+            adapter.Fill(tabla);
+            return tabla;
+            }
         }
     }
 }
--- TP7_GRUPO_24/Sucursal.cs
+++ TP7_GRUPO_24/Sucursal.cs
@@ -13,18 +13,16 @@ namespace TP7_GRUPO_24
         private string _NombreSucursal;
         private string _DescripcionSucursal;
         private string _URL_Imagen_Sucursal;
+        private int _Id_ProvinciaSucursal; // Agregado.
 
 
-        public Sucursal()
-        {
-
-        }
+        public Sucursal(){}
         public Sucursal(string NombreSucursal)
         {
             _NombreSucursal = NombreSucursal;
         }
 
-        public Sucursal(int idSucursal, string NombreSucursal ,string DescripcionSucursal, string URL_Imagen_Sucursal)
+        public Sucursal(int idSucursal, string NombreSucursal, string DescripcionSucursal, string URL_Imagen_Sucursal)
         {
             _idSucursal = idSucursal;
             _NombreSucursal = NombreSucursal;
@@ -32,52 +30,43 @@ namespace TP7_GRUPO_24
             _URL_Imagen_Sucursal = URL_Imagen_Sucursal;
         }
 
+            public Sucursal(int idSucursal, string NombreSucursal ,string DescripcionSucursal, string URL_Imagen_Sucursal, int Id_ProvinciaSucursal)
+        {
+            _idSucursal = idSucursal;
+            _NombreSucursal = NombreSucursal;
+            _DescripcionSucursal = DescripcionSucursal;
+            _URL_Imagen_Sucursal = URL_Imagen_Sucursal;
+            _Id_ProvinciaSucursal = Id_ProvinciaSucursal; 
+        }
+
         public int idSucursal
         {
-            get
-            {
-                return _idSucursal;
-            }
-            set
-            {
-                _idSucursal = value;
-            }
-        }
+            get {return _idSucursal;}
+            set { _idSucursal = value;}
+        }   
 
         public string NombreSucursal
         {
-            get
-            {
-                return _NombreSucursal;
-            }
-            set
-            {
-                _NombreSucursal = value;
-            }
+            get { return _NombreSucursal; }
+            set { _NombreSucursal = value;}
         }
 
         public string DescripcionSucursal
         {
-            get
-            {
-                return _DescripcionSucursal;
-            }
-            set
-            {
-                _DescripcionSucursal = value;
-            }
+            get {return _DescripcionSucursal;}
+            set {_DescripcionSucursal = value;}
         }
 
         public string URL_Imagen_Sucursal
         {
-            get
-            {
-                return _URL_Imagen_Sucursal;
-            }
-            set
-            {
-                _URL_Imagen_Sucursal = value;
-            }
+            get {return _URL_Imagen_Sucursal;}
+            set {_URL_Imagen_Sucursal = value;}
+        }
+
+        public int Id_ProvinciaSucursal
+        {
+            get { return _Id_ProvinciaSucursal; }
+            set { _Id_ProvinciaSucursal = value; }
         }
     }
 }
